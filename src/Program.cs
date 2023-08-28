1cba383d0e7a5faace049e5f49017fbd96aa7aa1
sekarfeb <sekarfeb@gmail.com>
2023-08-28 16:49:38 +0530

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -46,7 +46,7 @@ record Album(int Id, string Title, string Artist, double Price, string Image_url
             new Album(2, "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
             new Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
             new Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
-            new Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
+            new Album(5, "Lock Down Your Love - My Changes", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
             new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
          };
 
